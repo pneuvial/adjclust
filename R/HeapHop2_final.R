3e5521f73dbeeb4e4bc978e7c3c7dc0d01a60dfa
Shubham Chaturvedi <shubham.link@gmail.com>
2017-06-22 15:23:56 +0530

    Added new functions HeapHop and HeapHop2_final over original functions

---
+++ R/HeapHop2_final.R
@@ -0,0 +1,21 @@
+HeapHop2_final <- function(mat, p, h, NbCLasses){
+  p <- ncol(mat)
+  
+  x <- .Call("OneDiagBand",mat, as.integer(h))
+  
+  resP <- adjclust:::HeapHop2(x, p, h, 1)
+  
+  gains <- t(resP)[,3]
+  res <- t(resP)[,1:2]
+  height <- cumsum(gains)
+  traceW <- cbind(c(1:(p-1)),rev(height))
+  tree <- list(traceW  = traceW,
+               gains   = gains,
+               merge   = res,
+               height  = height,
+               seqdist = height,
+               order   = 1:p,
+               labels  = paste("",1:p))
+  class(tree) <- "hclust"
+  return(tree)
+}
