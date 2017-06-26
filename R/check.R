d8caff6df80ee99923f193112a251af3b2169054
Shubham Chaturvedi <shubham.link@gmail.com>
2017-06-27 04:28:33 +0530

    Update check.R

--- R/check.R
+++ R/check.R
@@ -17,7 +17,8 @@ modify <- function(m) {
   
   if ("dist" %in% class(m)) {
   d <- as.matrix(m)
-  mat <- 1 - 0.5*(d^2)
+ #  mat <- 1 - 0.5*(d^2)
+  mat <- 1 - 0.5*d   
   } else {
     if (all(diag(m) == 1))
     {
