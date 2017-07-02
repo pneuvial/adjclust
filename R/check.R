44033bc5299fb07a596d758345824ce48c318d7c
Shubham Chaturvedi <shubham.link@gmail.com>
2017-07-02 05:40:05 +0530

    phase1 improvements

--- R/check.R
+++ R/check.R
@@ -1,14 +1,15 @@
 modify <- function(m) {
-
-  if (!(is.matrix(m) || ("dist" %in% class(m))))
+  
+  t <- is.matrix(m)
+  if (!(t || ("dist" %in% class(m))))
     stop("Input must be a similarity matrix or a dist object")
 
-  if (is.matrix(m)) {
+  if (t) {
     if (!(nrow(m) == ncol(m)))
       stop("Input matrix is not a square matrix")
     if (!is.numeric(m))
       stop("Input matrix is not numeric")
-    if (!(sum(m == t(m)) == (nrow(m)^2)))
+    if (!(isSymmetric.matrix(m)))
       stop("Input matrix is not symmetric")
   }
 
@@ -17,8 +18,7 @@ modify <- function(m) {
   
   if ("dist" %in% class(m)) {
   d <- as.matrix(m)
- #  mat <- 1 - 0.5*(d^2)
-  mat <- 1 - 0.5*d   
+  mat <- 1 - 0.5*(d^2)
   } else {
     if (all(diag(m) == 1))
     {
@@ -28,7 +28,7 @@ modify <- function(m) {
           m1 <- sweep(m, 1, diag(m))
           l <- max(m1)  
           if (l<0) l <- 0
-          diag(m) <- diag(m) + l + 0.1   #Taking epsilon = 0.1 
+          diag(m) <- diag(m) + l + (0.1*l)   #Taking epsilon = 0.1 
         }
        mat <- makeDiagOne(m)
      }
