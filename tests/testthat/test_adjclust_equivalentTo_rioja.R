ce522a6a5c4a5095e3f4cc750ba91ec5e77d403d
Shubham Chaturvedi <shubham.link@gmail.com>
2017-07-11 02:46:59 +0530

    corrections

--- tests/testthat/test_adjclust_equivalentTo_rioja.R
+++ tests/testthat/test_adjclust_equivalentTo_rioja.R
@@ -10,17 +10,20 @@ check_rioja <- function() {
 
 test_that("rioja and adjClustBand_heap with full band give idenctical results on simmatrix.rda", {
   check_rioja()
-  data("simmatrix", package="adjclust")
   
+  data("simmatrix", package="adjclust")  
   sim <- simmatrix
   p <- nrow(sim)
       
-  dist <- 2 - (2*sim)
-  dist <- as.dist(dist)
+  dis_sq <- 2 - (2*sim)
+  dis <- sqrt(dis_sq)
+  
+  dis_sq <- as.dist(dis_sq)
+  dis <- as.dist(dis_sq)
   
   fit1 <- adjclust:::adjClustBand_heap(sim, p-1)
-  fit2 <- chclust(dist,method="coniss")
-  fit3 <- adjclust:::adjClustBand_heap(dist, p-1)
+  fit2 <- chclust(dis_sq,method="coniss")
+  fit3 <- adjclust:::adjClustBand_heap(dis, p-1)
   
   expect_equal(fit1$merge, fit2$merge)
   expect_equal(fit3$merge, fit2$merge)
