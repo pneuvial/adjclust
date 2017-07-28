6e299999fc0e92876f8297dcbf92d306da2484a3
Shubham Chaturvedi <shubham.link@gmail.com>
2017-07-28 14:30:55 +0530

    added function to create matL and RmatR

---
+++ src/CSparseMatLR.c
@@ -0,0 +1,114 @@
+//C functions
+
+#include <stdio.h>
+#include <R.h>
+#include <Rinternals.h>
+#include <Rdefines.h>
+#include <Rmath.h>
+
+
+int min(int a,int b)
+{
+  if (a <= b)  return a;
+  else  return b; 
+} 
+
+SEXP CSparseMatL(SEXP X, SEXP Rp, SEXP Rh)
+{
+  int *p, *h, i ,j;
+  double *xptr, *outptr;
+  xptr = REAL(X);
+  p = INTEGER(Rp);
+  h = INTEGER(Rh);
+
+  SEXP out;
+  PROTECT(out = allocMatrix(REALSXP, *p, *h));
+  outptr = REAL(out);
+
+  // intialize "out" matrix values to zero
+  for(i = 0; i < *p; i++)
+  {
+    for(j = 0; j < *h ; j++)
+    {
+        outptr[i + (*p)*j] = 0;      
+    }
+  }
+
+  for(i = 0; i < *p; i++)
+  {
+    for(j = 0; j < *h ; j++)
+    {
+      if( (j < *h)&&(i+j+1 < *p) )
+        outptr[i + (*p)*j] = xptr[i+j+1 +(*p)*(j+1)];      
+    }
+  }
+
+  UNPROTECT(1);
+  return(out);
+}
+
+
+SEXP CSparseRmatR(SEXP X, SEXP Rp, SEXP Rh)
+{
+  int *p, *h, i ,j;
+  double *xptr, *outptr;
+  xptr = REAL(X);
+  p = INTEGER(Rp);
+  h = INTEGER(Rh);
+
+// int k,l; double *tempptr;
+/*  
+  SEXP temp;
+  PROTECT(temp = allocVector(REALSXP, (*p)*(*h) ));
+  tempptr = REAL(temp);
+*/
+
+  SEXP out;
+  PROTECT(out = allocMatrix(REALSXP, *p, *h) );
+  outptr = REAL(out);
+
+/*
+  // intialize "out" matrix values to zero
+  for(j = 0; j < *p; j++)
+  {
+    for(k = 0; k < *h ; k++)
+    {
+      outptr[j + (*p)*k] = 0;      
+    }
+  }
+
+	l=0;
+  for(i = 1; i < *p ; i++)
+  {
+    j = i;
+    k = 1;
+    while( (j<*p)&&(k<*h+1) )
+    {
+      tempptr[l++] = xptr[j + (*p)*k];
+      j++;k++;
+    }
+  }
+  l--;
+
+  for(i = 0; i < *p ; i++)
+  {
+  	for(j = 0 ;j < min(*h,*p-1-i); j++)
+  	{
+      outptr[i + (*p)*j] = tempptr[l--];
+  	}
+  }
+*/
+  
+  for(i = *p-1 ; i >= 0 ; i--)
+  {
+    for(j = 1; j <= *h ; j++)
+    {
+        outptr[(*p - 1 - i) + (*p)*(j-1)] = xptr[i + (*p)*(j)];      
+    }
+  }
+
+
+  UNPROTECT(1);
+  return(out);
+
+}
