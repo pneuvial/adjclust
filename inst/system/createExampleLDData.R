21597ba43faf52bbd46af99de5fe32f20f9dcda7
Shubham Chaturvedi <shubham.link@gmail.com>
2017-07-28 14:37:34 +0530

    added compress argument

--- inst/system/createExampleLDData.R
+++ inst/system/createExampleLDData.R
@@ -8,5 +8,5 @@ ld.ceph <- ld(ceph.1mb, stats=c("D.prime", "R.squared"), depth=100)
 R2.100 <- ld.ceph$R.squared
 Dprime.100 <- ld.ceph$D.prime
 
-save(R2.100, file="data/R2.100.rda")
-save(Dprime.100, file="data/Dprime.100.rda")
+save(R2.100, file="data/R2.100.rda", compress="xz")
+save(Dprime.100, file="data/Dprime.100.rda", compress="xz")
