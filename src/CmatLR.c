fb494859a587eaa4351bb3611dde3cce18a7209b
Shubham Chaturvedi <shubham.link@gmail.com>
2017-07-28 14:13:59 +0530

    added functions for matL and RmatR calculation

---
+++ src/CmatLR.c
@@ -0,0 +1,111 @@
+//C functions to create matL and RmatR matrix
+
+#include <stdio.h>
+#include <R.h>
+#include <Rinternals.h>
+#include <Rdefines.h>
+#include <Rmath.h>
+
+
+int minm(int a,int b)
+{
+  if (a <= b)  return a;
+  else  return b; 
+} 
+
+SEXP CmatL(SEXP X, SEXP Rp, SEXP Rh)
+{
+  int *p, *h, i ,j, k;
+  double *xptr, *outptr;
+  xptr = REAL(X);
+  p = INTEGER(Rp);
+  h = INTEGER(Rh);
+
+  SEXP out;
+  PROTECT(out = allocMatrix(REALSXP, *p, *h));
+  outptr = REAL(out);
+
+  // intialize "out" matrix values to zero
+  for(j = 0; j < *p; j++)
+  {
+    for(k = 0; k < *h ; k++)
+    {
+      outptr[j + (*p)*k] = 0;      
+    }
+  }
+
+  for(i=0; i < *p; i++)
+  {
+  	for(j = i+1 ; j <= minm(i + *h,*p - 1); j++)
+  	{
+      outptr[i + (*p)*(j-i-1)] = xptr[i + (*p)*j];
+  	}
+  }
+
+  UNPROTECT(1);
+  return(out);
+}
+
+
+SEXP CRmatR(SEXP X, SEXP Rp, SEXP Rh)
+{
+  int *p, *h, j, k;
+  double *xptr, *outptr;
+  xptr = REAL(X);
+  p = INTEGER(Rp);
+  h = INTEGER(Rh);
+
+/*  
+  SEXP temp;
+  PROTECT(temp = allocVector(REALSXP, (*p)*(*h) ));
+  tempptr = REAL(temp);
+*/
+
+  SEXP out;
+  PROTECT(out = allocMatrix(REALSXP, *p, *h) );
+  outptr = REAL(out);
+
+  // intialize "out" matrix values to zero
+  for(j = 0; j < *p; j++)
+  {
+    for(k = 0; k < *h ; k++)
+    {
+      outptr[j + (*p)*k] = 0;      
+    }
+  }
+
+/*
+	k=0;
+  for(i = 0; i < *p ; i++)
+  {
+  	for(j = i+1 ; j <= min(i + *h, *p-1); j++)
+  	{
+      tempptr[k++] = xptr[i + (*p)*j];
+  	}
+  }
+  k--;
+
+  for(i = 0; i < *p ; i++)
+  {
+  	for(j = 0 ;j < min(*h,*p-1-i); j++)
+  	{
+      outptr[i + (*p)*j] = tempptr[k--];
+  	}
+  }
+*/
+  for(j = 0; j < *p; j++)
+  {
+    for(k = 0; k < *h ; k++)
+    {
+      if( (*p-2-k-j) >= 0 )
+      { 
+      outptr[j + (*p)*k] = xptr[(*p-1-j) + (*p)*(*p-2-k-j)];
+      }      
+    }
+  }
+  
+  
+  UNPROTECT(1);
+  return(out);
+
+}
