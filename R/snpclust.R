aec5d419892268b95f503490d13b61159b80e39c
Shubham Chaturvedi <shubham.link@gmail.com>
2017-08-18 05:10:04 +0530

    added snpclust.R

---
+++ R/snpclust.R
@@ -0,0 +1,86 @@
+#' Constrained Hierarchical Agglomerative Clustering of Single Nucleotide Polymorphisms
+#' 
+#' Function to perform adjacency-constrained hierarchical agglomerative clustering of Single Nucleotide Polymorphisms
+#' 
+#' \code{snpclust} performes constrained hierarchichal agglomerative clustering of single nucleotide 
+#' polymorphisms.Constrained Hierarchical Agglomerative Clustering is hierarchical agglomerative
+#' clustering in which each observation is associated to a position, and the clustering is 
+#' constrained so as only adjacent clusters are merged.
+#' 
+#' @param x Either a genotype matrix of class snpStats::SnpMatrix/base::matrix or a linkage disequlibrium matrix of 
+#' class Matrix::dgCMatrix
+#' @param h band width. It is assumed that the similarity between two items is 0 when these items are at a 
+#' distance of more than band width h
+#' @param stats A character vector specifying the linkage disequilibrium measures to be calculated. This should contain one or 
+#' more of the strings: "LLR", "OR", "Q", "Covar", "D.prime", "R.squared" or "R"
+#' 
+#' @return Function \code{snpclust} returns an object of class \code{\link[stats]{hclust}}.  
+#' 
+#' @examples
+#' library(snpStats)
+#' data(testdata)
+#' 
+#' #Input as snpStats::SnpMatrix
+#' fit1 <- snpclust(Autosomes[1:200, 1:5], 3, "R.squared")
+#' 
+#' #Input as base::matrix
+#' fit2 <- snpclust(as.matrix(Autosomes[1:200, 1:5]), 3, "R.squared")
+#' 
+#' #Input as Matrix::dgCMatrix
+#' ld <- ld(Autosomes[1:200, 1:5], depth=3, stats="R.squared")
+#' fit3 <- snpclust(ld, 3)
+#' 
+#' @export
+#' 
+#' @importFrom methods as
+#' @importFrom snpStats ld
+snpclust <- function(x ,h ,stats) {
+  
+  if (!is.numeric(h))
+    stop("h should be numeric")
+  
+  CLASS <- c("dgCMatrix", "matrix", "SnpMatrix")
+  classcheck <- pmatch(class(x), CLASS)
+  
+  if(is.na(classcheck))
+    stop("Input matrix class not supported")
+  if(classcheck == -1)
+    stop("Ambiguous matrix class")
+  class <- CLASS[classcheck]  
+
+  if(class != "dgCMatrix" )
+  {
+    p <- ncol(x)
+    if (h >= p)
+      stop("h should be strictly less than p")
+    
+    if (class == "matrix") {
+      rownames(x) <- 1:nrow(x)
+      colnames(x) <- 1:ncol(x)
+      x <- as(x, "SnpMatrix")
+    }
+    
+    if (missing(stats)) 
+      stop("LD stats must be specified")
+    
+    STATS <- c("LLR", "OR", "Q", "Covar", "D.prime", "R.squared", "R")
+    statscheck <- pmatch(stats, STATS)
+    
+    if(is.na(statscheck))
+      stop("Invalid stats")
+    if(statscheck == -1)
+      stop("Ambiguous stats")
+    stats <- STATS[statscheck] 
+    
+    x <- ld(x, stats = stats, depth = h)
+    x <- round(x, digits=10)
+    
+    if (any(is.na(x)))
+      stop("Missing value(s) or NaN(s) generated in ld calculation.Please check the input for missing observations")    
+  }
+  
+  diag(x) <- 1
+  res <- adjClustBand_heap(x, "similarity", h, 1, FALSE)
+  
+  return(res)
+}
