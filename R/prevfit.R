1d04c3c46cb68f5dd4be70a5fdb9884abdbd439e
Shubham Chaturvedi <shubham.link@gmail.com>
2017-08-18 05:11:20 +0530

    added prevfit.R

---
+++ R/prevfit.R
@@ -0,0 +1,9 @@
+#' Results obtained from primitive version of adjClustBand_heap function
+#'
+#' A hclust object obtained from primitve version of adjClustBand_heap function
+#'
+#' @format An object of class hclust.
+#'
+#' @source primitive version of adjClustBand_heap function.
+#'
+"prevfit"
