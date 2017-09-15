ec9dc8381989be36f7e60f730dccbac18d86696a
Pierre Neuvial <pneuvial@gmail.com>
2017-09-15 15:56:12 +0200

    * Cleanups in Hi-C and LD vignettes and corresponding tests

--- R/snpClust.R
+++ R/snpClust.R
@@ -17,6 +17,7 @@
 #' @return Function \code{snpClust} returns an object of class \code{\link[stats]{hclust}}.  
 #' 
 #' @examples
+#' ## a very small example
 #' library(snpStats)
 #' data(testdata)
 #' 
@@ -30,57 +31,62 @@
 #' ld <- ld(Autosomes[1:200, 1:5], depth=3, stats="R.squared")
 #' fit3 <- snpClust(ld, 3)
 #' 
+#' ## A less small example
+#' 
 #' @export
 #' 
 #' @importFrom methods as
 #' @importFrom snpStats ld
 snpClust <- function(x ,h ,stats) {
-  
-  if (!is.numeric(h))
-    stop("h should be numeric")
-  
-  CLASS <- c("dgCMatrix", "matrix", "SnpMatrix")
-  classcheck <- pmatch(class(x), CLASS)
-  
-  if(is.na(classcheck))
-    stop("Input matrix class not supported")
-  if(classcheck == -1)
-    stop("Ambiguous matrix class")
-  class <- CLASS[classcheck]  
-
-  if(class != "dgCMatrix" )
-  {
-    p <- ncol(x)
-    if (h >= p)
-      stop("h should be strictly less than p")
     
-    if (class == "matrix") {
-      rownames(x) <- 1:nrow(x)
-      colnames(x) <- 1:ncol(x)
-      x <- as(x, "SnpMatrix")
+    if (!is.numeric(h))
+        stop("h should be numeric")
+    
+    CLASS <- c("dgCMatrix", "matrix", "SnpMatrix")
+    classcheck <- pmatch(class(x), CLASS)
+    
+    if(is.na(classcheck))
+        stop("Input matrix class not supported")
+    if(classcheck == -1)
+        stop("Ambiguous matrix class")
+    class <- CLASS[classcheck]  
+    
+    if(class != "dgCMatrix" ) {
+        p <- ncol(x)
+        if (h >= p)
+            stop("h should be strictly less than p")
+        
+        if (class == "matrix") {
+            rownames(x) <- 1:nrow(x)
+            colnames(x) <- 1:ncol(x)
+            x <- as(x, "SnpMatrix")
+        }
+        
+        if (missing(stats)) 
+            stop("LD stats must be specified")
+        
+        STATS <- c("LLR", "OR", "Q", "Covar", "D.prime", "R.squared", "R")
+        statscheck <- pmatch(stats, STATS)
+        
+        if(is.na(statscheck))
+            stop("Invalid stats")
+        if(statscheck == -1)
+            stop("Ambiguous stats")
+        stats <- STATS[statscheck] 
+        
+        x <- ld(x, stats = stats, depth = h)
+        x <- round(x, digits=10)
+        if (any(is.na(x))) {
+            ww <- which(is.na(as.matrix(x)), arr.ind=TRUE)
+            print(utils::str(ww))
+            stop("Missing value(s) or NaN(s) in LD estimates (indices are printed above)")
+        }
     }
+    if (any(is.na(x))) {
+        stop("Missing value(s) or NaN(s) not allowed in similarity matrix.")    
+    }
+    diag(x) <- 1
+    res <- adjClust(x, "similarity", h, 1, FALSE)
     
-    if (missing(stats)) 
-      stop("LD stats must be specified")
-    
-    STATS <- c("LLR", "OR", "Q", "Covar", "D.prime", "R.squared", "R")
-    statscheck <- pmatch(stats, STATS)
-    
-    if(is.na(statscheck))
-      stop("Invalid stats")
-    if(statscheck == -1)
-      stop("Ambiguous stats")
-    stats <- STATS[statscheck] 
-    
-    x <- ld(x, stats = stats, depth = h)
-    x <- round(x, digits=10)
-    
-    if (any(is.na(x)))
-      stop("Missing value(s) or NaN(s) generated in ld calculation. Please check the input for missing observations.")    
-  }
-  
-  diag(x) <- 1
-  res <- adjClust(x, "similarity", h, 1, FALSE)
-  
-  return(res)
+    return(res)
 }
