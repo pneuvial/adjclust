1454c29a0f790ef9d9cf7f9aebd51fcd37b2f6b1
Pierre Neuvial <pneuvial@gmail.com>
2017-09-15 15:57:05 +0200

    * Added test script for snpClust

---
+++ tests/testthat/test_snpClust_NA-in-LD.R
@@ -0,0 +1,54 @@
+library("adjclust")
+library("snpStats")
+
+context("Case of NA values in LD estimates")
+
+test_that("NA values in LD estimates gives an error in 'snpClust'", {
+    data("ld.example", package="snpStats")
+    p <- ncol(ceph.1mb)
+    h <- p-1
+    
+    ld.ceph <- ld(ceph.1mb, stats="R.squared", depth=h)
+    ## In this unrealistically small example with only 90 subjects, it turns out
+    ## that one of the LD estimates is NA due to the lack of genetic diversity in
+    ## the sample:
+    expect_true(any(is.na(ld.ceph)))
+    expect_error(snpClust(ceph.1mb, h=h, stats="R.squared"))
+    expect_error(snpClust(ld.ceph, h=h))
+})
+
+## One way to correct this is to drop one of the incriminated SNPs
+test_that("Dropping a SNP yielding NA values in LD fixes the NA problem", {
+    geno <- ceph.1mb[, -316]  ## drop one SNP leading to one missing LD value
+    p <- ncol(geno)
+    h <- p-1
+    
+    ld.ceph <- ld(geno, stats="R.squared", depth=h)
+    expect_true(all(!is.na(ld.ceph)))
+    
+    ## avoid LD values slightly greater than 1
+    ld.ceph10 <- round(ld.ceph, digits=10)  
+    
+    fit <- snpClust(geno, h=h, stats="R.squared")
+    fit10 <- snpClust(ld.ceph10, h=h, stats="R.squared")
+    expect_identical(fit, fit10)
+})
+
+## Another way to correct this is to modify the genotype of a single sample
+test_that("Modifying one genotypes also fixes the NA problem", {
+    data("ld.example", package="snpStats")
+    p <- ncol(ceph.1mb)
+    h <- p-1
+    nSamples <- nrow(ceph.1mb)
+
+    ceph.1mb[4,286]@.Data[1,1] <- as.raw(3) ## to avoid NaNs
+    ld.ceph <- ld(ceph.1mb, stats="R.squared", depth=h)
+    expect_true(all(!is.na(ld.ceph)))
+    
+    ## avoid LD values slightly greater than 1
+    ld.ceph10 <- round(ld.ceph, digits=10)  
+    
+    fit <- snpClust(ceph.1mb, h=h, stats="R.squared")
+    fit10 <- snpClust(ld.ceph10, h=h, stats="R.squared")
+    expect_identical(fit, fit10)
+})
