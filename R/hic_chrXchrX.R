22da195c9d8ccda6a4aadf81097af2e3d29c0b98
Pierre Neuvial <pneuvial@gmail.com>
2017-09-15 11:45:25 +0200

    Edits to Hi-C vignette (+ side effects)

--- R/hic_chrXchrX.R
+++
@@ -1,9 +0,0 @@
-#' The HTCexp object named "obj" contains intrachromosmal contact map of chromosome X with itself
-#'
-#' An HTCexp object named "obj" is obtained from HiC dataset published by Dixon et al (GSE35156) that contains all genome-wide contact maps.
-#'
-#' @format An object of class HTCexp.
-#'
-#' @source http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE35156
-#'
-"obj"
--- tests/testthat/test_hicClust.R
+++ tests/testthat/test_hicClust.R
@@ -6,36 +6,26 @@ context("Checking the consistency of the results of hicclust function across var
 test_that("hicClust function identical results for the same data in all three inputs formats", {
 
   #case1: Input as HiTC::HTCexp object
-  data("hic_chrXchrX", package="adjclust")
-  h <- 3881
-  fit1 <- hicClust(obj, h)
+  data("hic_imr90_40_XX", package="adjclust")
+  fit1 <- hicClust(hic_imr90_40_XX)
   
   #case2: Input as Matrix::dsCMatrix contact map
-  mat <- intdata(obj) 
-  fit2 <- hicClust(mat, h)
+  mat <- intdata(hic_imr90_40_XX) 
+  fit2 <- hicClust(mat)
   
-  #case3: Input as text file
-  V3 <- mat@x
   V1 <- mat@Dimnames[[1]][mat@i+1]          #loci1names
   V2 <- rep(mat@Dimnames[[2]], diff(mat@p)) #loci2names
-  
-  l1 <- unique(c(as.numeric(V1), as.numeric(V2)))  
-  
-  l2 <- setdiff(c(72037:75918), l1) 
-  V1 <- append(V1, l2)
-  V2 <- append(V2, l2)
-  V3 <- append(V3, rep(0, length(l2)))
+  V3 <- mat@x
   
   content <- cbind(as.numeric(V1), as.numeric(V2), as.numeric(V3))
   
   tf <- tempfile(fileext = ".txt")
   write.table(content, tf, sep = " ", col.names = FALSE, row.names = FALSE)
-  fit3 <- hicClust(tf, h, sep = " ")  
+  fit3 <- hicClust(tf, sep = " ")  
   
   expect_equal(fit1$merge, fit2$merge)
   expect_equal(fit1$height, fit2$height)  
   
   expect_equal(fit2$merge, fit3$merge)
   expect_equal(fit2$height, fit3$height)  
-  
 })
