4160773c7244f69bedad9a9011102c3054b4bd35
Nathalie Villa-Vialaneix <nathalie.villa-vialaneix@inra.fr>
2017-11-27 14:39:38 +0100

    added various options for heights of the dendrogram and documented them

--- R/chac.R
+++ R/chac.R
@@ -51,14 +51,41 @@ summary.chac <- function(object, ...) {
 #' merges). When \code{\link{plot.chac}} is called with 
 #' \code{mode = "corrected"}, a correction is applied to original heights so as
 #' to have only non decreasing merges). It does not change the result of the 
-#' clustering, only the look of the dendrogram for easier interpretation. When
-#' \code{\link{plot.chac}} is called with \code{mode = "inertia"}, heights of
-#' the dendrogram correspond to the cluster inertia instead of the cluster
-#' linkage value. This also ensures non decreasing merges.
+#' clustering, only the look of the dendrogram for easier interpretation.\cr\cr
+#' Other modes are provided that correspond to different alternatives
+#' described in Grimm (1987): \itemize{
+#' \item in \code{mode = "within-disp"}, heights correspond to whithin-cluster
+#' dispersion, \emph{i.e.}, for a corresponding cluster, its height is 
+#' \deqn{I(C) = \sum_{i \in C} d(i,g_C)} where \eqn{d} is the dissimilarity 
+#' used to cluster objects and \eqn{g_C} is the center of gravity of cluster
+#' \eqn{C}. In this case, heights are always non decreasing;
+#' \item in \code{mode = "total-disp"}, heights correspond to the sum of all
+#' whithin-cluster dispersions. It is obtained from \code{mode = "standard"} by
+#' the cumulartive sum of its heights. In this case, heights are always
+#' non decreasing;
+#' \item in \code{mode = "average-disp"}, heights correspond to the 
+#' whithin-cluster dispersion divided by the cluster size. In this case, there 
+#' is no guaranty that the heights are non decreasing. When reversals are 
+#' detected, a warning is printed to advice the user to change the mode of the
+#' representation.
+#' }\cr
+#' Grimm (1987) indicates that heights as provided by 
+#' \code{mode = "within-disp"} are highly dependant on cluster sizes and that 
+#' the most advisable representation is the one provided by 
+#' \code{mode = "total-disp"}.
+#' @references { Grimm, E.C. (1987) CONISS: a fortran 77 program for
+#' stratigraphically constrained analysis by the method of incremental sum of
+#' squares. \emph{Computer & Geosciences}, \strong{13}(1), 13-35. }
+#' @return The function \code{plot.chac} displays the dendrogram and 
+#' additionally invisibly returns an object of class 
+#' \code{\link[stats]{dendrogram}} with heights as specified by the user through
+#' the option \code{mode}.
 #' @export
-#' @importFrom graphics plot
-#' @importFrom stats as.dendrogram
-plot.chac <- function(x, y, ..., mode = c("standard", "corrected", "inertia")) {
+#' @importFrom graphics plot segments
+#' @importFrom stats as.dendrogram cutree
+plot.chac <- function(x, y, ..., 
+                      mode = c("standard", "corrected", "total-disp", 
+                               "within-disp", "average-disp")) {
   mode <- match.arg(mode)
   args <- list(...)
   if (is.null(args$type)) args$type <- "triangle"
@@ -66,8 +93,8 @@ plot.chac <- function(x, y, ..., mode = c("standard", "corrected", "inertia")) {
   
   if (mode == "standard") {
     if (any(diff(x$height) < 0)) 
-      warning(paste0("\nDecreasing merges: ",
-                     "'mode = 'corrected' or 'inertia' might be more relevant."))
+      warning(paste0("\nDetected reversals in dendrogram: ",
+                     "mode = 'corrected', 'within-disp' or 'total-disp' might be more relevant."))
     if (is.null(args$ylim)) args$ylim <- range(x$height)
   } else if (mode == "corrected") {
     res_diagnose <- diagnose(x, graph = FALSE, verbose = FALSE)
@@ -79,14 +106,33 @@ plot.chac <- function(x, y, ..., mode = c("standard", "corrected", "inertia")) {
     to_add <- rowSums(to_add)
     x$height <- x$height + to_add
     ## note: remaining decreasing gains due to numerical approximations
-  } else if (mode == "inertia") {
+  } else if (mode == "total-disp") {
+    x$height <- cumsum(x$height)
+  } else if (mode == "within-disp" | mode == "average-disp") {
     to_correct <- which((x$merge[ ,1] > 0) | (x$merge[ ,2] > 0))
     for (ind in to_correct) {
       clusters <- x$merge[ind, ]
       clusters <- clusters[clusters > 0]
       x$height[ind] <- x$height[ind] + sum(x$height[clusters])
     }
-    args$ylab <- "cluster inertia"
+    if (mode == "average-disp") {
+      # search for current cluster size
+      out <- sapply((length(x$height) + 1):1, function(num) {
+        tmp <- table(table(cutree(as.hclust(x), k = num)))
+        res <- rep(0, length(x$height) + 1)
+        res[as.numeric(names(tmp))] <- tmp
+        return(res)
+      })
+      out <- apply(out, 1, diff)
+      sizes <- unlist(apply(out, 1, function(acol) which(acol > 0)))
+      sizes <- as.numeric(sizes)
+      x$height <- x$height / sizes
+      if (any(diff(x$height) < 0)) 
+        warning(paste0("\nDetected reversals in dendrogram: ",
+                       "mode = 'corrected', 'within-disp' or 'total-disp' might be more relevant."))
+      if (is.null(args$ylim)) args$ylim <- range(x$height)
+    }
+    args$ylab <- "within-cluster dispersion"
   }
   
   args$x <- as.dendrogram(as.hclust(x))
@@ -102,7 +148,8 @@ plot.chac <- function(x, y, ..., mode = c("standard", "corrected", "inertia")) {
     segments(rep(-0.25, length(x0)), y0, rep(0.25, length(x0)), col = "darkred")
     segments(rep(-0.25, length(x0)), y1, rep(0.25, length(x0)), col = "darkred")
   }
-  invisible(NULL)
+  
+  invisible(as.dendrogram(as.hclust(x)))
 }
 
 #' @export
@@ -117,10 +164,11 @@ diagnose <- function(x, ...) {
 #' not. Default to \code{TRUE}
 #' @param verbose (logical) whether to print a summary of the result or not.
 #' Default to \code{TRUE}
-#' @details \code{\link{diagnose}} invisibly exports a data frame with the 
+#' @return \code{\link{diagnose}} invisibly exports a data frame with the 
 #' numbers of decreasing merges described by the labels of the clusters being
 #' merged at this step and at the previous one, as well as the corresponding
 #' merge heights.
+#' @importFrom graphics axis points
 #' @export
 diagnose.chac <- function(x, graph = TRUE, verbose = TRUE) {
   diff_heights <- diff(x$height)
