544a6e2ae57098825f843cc5ae3ec0b406d4439a
Nathalie Villa-Vialaneix <nathalie.villa-vialaneix@inra.fr>
2017-12-13 13:06:29 +0100

    added a test for messages and warnings with decreasing heights

---
+++ tests/testthat/test_warning_with_decreasing_height_plots.R
@@ -0,0 +1,16 @@
+library("adjclust")
+
+context("Check that the messages or warnings are produced for decreasing 
+        heights")
+
+test_that("'adjClust' returns a note when decreasing heights are produced and
+          warnings when such results are plotted with 'mode=standard' and
+          'mode=average-disp'", {
+  data("iris")
+  dissim <- dist(iris[ ,1:4])^2
+  sim <- 1-as.matrix(dissim)/2
+  expect_message(fit <- adjClust(sim), "merges with non increasing heights")
+  
+  expect_warning(plot(fit))
+  expect_warning(plot(fit, mode = "average-disp"))
+})
