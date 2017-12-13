72e959136ba27f03b53bdded706a038d0e7c859f
Nathalie Villa-Vialaneix <nathalie.villa-vialaneix@inra.fr>
2017-12-13 13:37:07 +0100

    added tests for cuttree

---
+++ tests/testthat/test_cuttree.R
@@ -0,0 +1,25 @@
+library("adjclust")
+
+context("Test cuttree in various situations (decreasing merges or not, k and/or
+        h given.")
+
+test_that("'cuttree' must ignore 'h' when reversals are present.", {
+  data("iris")
+  dissim <- dist(iris[ ,1:4])^2
+  sim <- 1-as.matrix(dissim)/2
+  fit <- adjClust(sim)
+  fit2 <- correct(fit)
+  
+  clust1 <- cutree(fit, k = 4)
+  expect_error(cutree(fit, h = 0.02))
+  clust2 <- cutree(fit, k = 4, h = 50)
+  expect_equal(clust1, clust2)
+  
+  clust3 <- cutree(fit2, k = 4)
+  expect_equal(clust1, clust3)
+  height4four <- (fit2$height[nrow(sim) - 3] + fit2$height[nrow(sim) - 4])/2
+  clust4 <- cutree(fit2, h = height4four)
+  expect_equal(clust1, clust4)
+  clust5 <- cutree(fit2, k = 4, h = 50)
+  expect_equal(clust1, clust5)
+})
