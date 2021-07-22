4a150ad330f34e0ded0598120893f16ec35a74a0
Nathalie Vialaneix <nathalie.vialaneix@inrae.fr>
2021-07-22 12:19:00 +0200

    updated Rcpp exports

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // matL_sparse
 arma::SpMat<double> matL_sparse(const arma::SpMat<double>& Csq, const int& h);
 RcppExport SEXP _adjclust_matL_sparse(SEXP CsqSEXP, SEXP hSEXP) {
